0214aa11370973686ac8f2f8a64aba9d9457394a
zuerock <70551180+zuerock@users.noreply.github.com>
2022-12-07 17:35:57 -0700

    Create gameDisplay.h

---
+++ lab9_project/gameDisplay.h
@@ -0,0 +1,31 @@
+#ifndef HANGMANDISPLAY
+#define HANGMANDISPLAY
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "display.h"
+
+typedef struct{
+    int16_t x;
+    int16_t y;
+} hangman_location_t;
+
+
+// Inits the hangman display, drawing the lines that form the board.
+void hangmanDisplay_init();
+
+void displayWords(uint32_t buttonVal, char *first, char *second, char *third, bool erase);
+
+// Performs the increment or decrement, given a touched point.
+void changeKeyColor(display_point_t point);
+
+void drawBody(int16_t incorrect, bool erase);
+
+hangman_location_t
+hangmanDisplay_getLocationFromPoint(display_point_t point);
+
+
+
+
+#endif /* TICTACTOEDISPLAY */
